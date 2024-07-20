e79ca0bd88600432d4e3d77d23846f60730bef9f
egorerkovich <erkovich.egor@yandex.by>
2024-07-20 21:22:09 +0300

    feat: add project structure

--- modsen-art-museum/src/main.tsx
+++ modsen-art-museum/src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { App } from './index';
 
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <h1>modsen art museum</h1>
+      <App />
     </React.StrictMode>
   );
 }
