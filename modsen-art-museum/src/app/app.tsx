8b92e37a6c5a8b1e3717e91c222829c2de1dc72e
egorerkovich <erkovich.egor@yandex.by>
2024-08-11 02:29:26 +0300

    fix: remove duplicates in app.tsx

--- modsen-art-museum/src/app/app.tsx
+++ modsen-art-museum/src/app/app.tsx
@@ -1,16 +1,22 @@
 import { DetailInfoPage, FavoritesPage, HomePage, Layout, NotFoundPage } from '@index';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 
+const routes = [
+  { path: 'home', element: <HomePage /> },
+  { path: 'details/:id', element: <DetailInfoPage /> },
+  { path: 'favorites', element: <FavoritesPage /> },
+  { path: '*', element: <NotFoundPage /> },
+];
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Navigate to="/home" />} />
-          <Route path="home" element={<HomePage />} />
-          <Route path="details/:id" element={<DetailInfoPage />} />
-          <Route path="favorites" element={<FavoritesPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
         </Route>
       </Routes>
     </Router>
